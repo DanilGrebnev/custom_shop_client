5bc6eaeaebce0afcb39e55f09859436078b2093d
Danil_Grebnev <t902hda@mail.ru>
2024-01-27 15:59:45 +0300

    add colors variables

--- src/widget/Header/ui/LinkList/index.tsx
+++ src/widget/Header/ui/LinkList/index.tsx
@@ -1,19 +1,40 @@
+'use client'
 import { NavigationRoutes } from '@/app/providers/NavigationRoutes'
 import Link from 'next/link'
+import clsx from 'clsx'
 import s from './s.module.scss'
+import { usePathname } from 'next/navigation'
+
+const a = [
+    { link: NavigationRoutes.main(), text: 'Главная' },
+    {
+        link: NavigationRoutes.shop(),
+        text: 'Магазин',
+    },
+    {
+        link: NavigationRoutes.profileMe(),
+        text: 'Личный кабинет',
+    },
+]
 
 export const LinkList = () => {
+    const pathname = usePathname()
+
     return (
         <ul className={s['page-list']}>
-            <li className={s['list-item']}>
-                <Link href={NavigationRoutes.main()}>Главная</Link>
-            </li>
-            <li className={s['list-item']}>
-                <Link href={NavigationRoutes.shop()}>Магазин</Link>
-            </li>
-            <li className={s['list-item']}>
-                <Link href={NavigationRoutes.profileMe()}>Личный кабинет</Link>
-            </li>
+            {a.map(({ link, text }) => {
+                return (
+                    <li
+                        key={link}
+                        className={s['list-item']}>
+                        <Link
+                            className={clsx({ [s.active]: pathname === link })}
+                            href={link}>
+                            {text}
+                        </Link>
+                    </li>
+                )
+            })}
         </ul>
     )
 }
