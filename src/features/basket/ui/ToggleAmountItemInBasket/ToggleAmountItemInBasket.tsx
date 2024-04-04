e175a9a40f7b3f053cb6efd592872d394af9adf7
Danil_Grebnev <t902hda@mail.ru>
2024-04-04 03:10:30 +0300

    feat: add delete product from basket if product couunt = 1

--- src/features/basket/ui/ToggleAmountItemInBasket/ToggleAmountItemInBasket.tsx
+++ src/features/basket/ui/ToggleAmountItemInBasket/ToggleAmountItemInBasket.tsx
@@ -1,12 +1,12 @@
 'use client'
 
-import { useEffect, useState } from 'react'
-import { memo } from 'react'
+import { memo, useEffect, useState } from 'react'
 
 import { AmountAdded } from '@/shared/ui/AmountAdded'
 import { Dialog } from '@/shared/ui/Modal'
 
 import {
+    useDeleteProductFromBasketByIdMutation,
     useGetCartQuery,
     useToggleItemAmountInBasketMutation,
 } from '../../model/api/basketApi'
@@ -24,15 +24,18 @@ export const ToggleAmountItemInBasket = memo((props: Props) => {
      * кол-ва товара, не дожидаясь ответа от сервера */
     const [amount, setAmount] = useState<number>(itemAmountInBasket)
     const [isOpenDialog, setIsOpenDialog] = useState<boolean>(false)
+
+    const [deleteProductFromBasket, { isLoading: isLoadingDeleteFromBasket }] =
+        useDeleteProductFromBasketByIdMutation()
+
     const { isFetching: isFetchingCart } = useGetCartQuery()
     const [
         toggleItemAmount,
-        { isLoading: isLoadingToggleItemAmount, isError },
+        { isLoading: isLoadingToggleItemAmount, isError: isErrorToggleAmount },
     ] = useToggleItemAmountInBasketMutation()
 
-    const loading = isLoadingToggleItemAmount || isFetchingCart
-    const disabled1 = currentItemAmount === itemAmountInBasket
-    const disabled2 = itemAmountInBasket === 1
+    const loading =
+        isLoadingToggleItemAmount || isFetchingCart || isLoadingDeleteFromBasket
 
     const increment = () => {
         // Оптимистично обновляем количество товара
@@ -47,6 +50,10 @@ export const ToggleAmountItemInBasket = memo((props: Props) => {
     }
 
     const decrement = () => {
+        if (itemAmountInBasket === 1) {
+            deleteProductFromBasket(cartItemId)
+            return
+        }
         setAmount((p) => p - 1)
         toggleItemAmount({
             id: cartItemId,
@@ -61,14 +68,13 @@ export const ToggleAmountItemInBasket = memo((props: Props) => {
     }, [itemAmountInBasket])
 
     useEffect(() => {
-        isError && setIsOpenDialog(true)
-    }, [isError])
+        isErrorToggleAmount && setIsOpenDialog(true)
+    }, [isErrorToggleAmount])
 
     return (
         <>
             <AmountAdded
-                disabled1={disabled1}
-                disabled2={disabled2}
+                disabled1={currentItemAmount === itemAmountInBasket}
                 value={amount}
                 disabled={loading}
                 increment={increment}
